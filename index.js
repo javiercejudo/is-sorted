bb499d2d7261e9c0ddfa505c828995b51fefe514
Javier Cejudo <javier@javiercejudo.com>
2015-11-28 02:11:34 +1100

    feat: more functional flavoured implementation

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var every2 = require('every2');
-var lte = require('lodash.lte');
-
-module.exports = function sorted (array, check) {
-  return every2(array, check || lte);
-};
+module.exports = require('unary-with-optional-callback')(require('every2'), require('lodash.lte'));
